0c6b465ff1fef0f1e9faad7a4fc5f75194e6a09b
Mikita Vishneuski <vishnevkiy.nikita@gmail.com>
2023-10-18 16:06:09 +0300

    2

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,6 +4,6 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return NumberUtils.isParsable(str) && NumberUtils.toDouble(str) >= 0.0;
+        return NumberUtils.isParsable(str) && NumberUtils.toDouble(str) > 0.0 ;
     }
 }
